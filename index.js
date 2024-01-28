2866c848fcb9aad1644ca1bba9da3366997a8ad3
CMBCAM <cameron.barradas@gmail.com>
2024-01-27 22:40:11 -0600

    added email, word, and phone detection

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ let name;
 let temp;
 let nextPage;
 let legitData;
-let yuckyPosts = [[]];
+let positive,culture,negative;
+//emails / phones
+let yucky = [];
+let interests = [[]]
 
 function searchData(){
     console.log(name);
@@ -40,22 +43,91 @@ function searchData(){
     //Loop through elements
     let wordArray;
     let currentWord;
+    legitData[4].caption += " 808-852-0200 ";
     for (let i = 0; i < size; i++){
+        let emails;
+        let phones;
         //check if a caption exists
         if(legitData[i].caption !==undefined){
-            wordArray = legitData[i].caption.split(/\s+/);
+            //this will be long
+            let count = 0;
+            for (let j = 0; j < culture.length; j++){ //culture only
+                if (legitData[i].caption.toLowerCase().indexOf(culture[j].toLowerCase()) !== -1){
+                    count++;
+                    console.log(culture[j]);
+                }
+                //console.log(count + " cultures");
+            }
+
+
+            /*wordArray = legitData[i].caption.split(/\s+/);
             for (let j = 0; j < wordArray.length;j++){
                 currentWord = wordArray[j]; //each word 
                 console.log(currentWord + ' ' + i);
-            }
+            }*/
+            //email address
+            emails = findEmailAddresses(legitData[i].caption);
+            phones = findPhoneNumber(legitData[i].caption);
+            //were there any emails
+            if(emails.length !==0){
+                yucky = yucky.concat(legitData[i]);
+            }  
+            else if(phones.length !== 0){
+                yucky = yucky.concat(legitData[i]);
+            }  
         }
     }
 
-    console.log(size);
+    console.log(yucky);
+}
+
+function parseWords(){
+
+}
+
+function findEmailAddresses(text) {
+    // Regular expression to match email addresses
+    let emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+    // Using the match method to find all matches in the text
+    let matches = text.match(emailPattern);
+    
+    return matches || [];
+}
+
+function findPhoneNumber(text) {
+    // Regular expression to match email addresses
+    let phonePattern = /\b(?:\+?\d{1,4}[\s\-]?)?(?:\(\d{1,4}\)[\s\-]?)?\d{1,4}[\s\-]?\d{4}\b/g;
+    // Using the match method to find all matches in the text
+    let matches;
+    matches = text.match(phonePattern);
+    
+    return matches || [];
 }
 
 async function fetchData() {
   try {
+    //all the filters first
+    fetch('filters/negative.txt')
+        .then(response => response.text())
+        .then(data => {
+            negative = data.split('\r\n');
+            console.log(negative);
+        })
+        .catch(error => console.error('Error loading the file:', error));
+    fetch('filters/positive.txt')
+        .then(response => response.text())
+        .then(data => {
+            positive = data.split('\r\n');
+            console.log(positive);
+        })
+        .catch(error => console.error('Error loading the file:', error));
+    fetch('filters/culture.txt')
+        .then(response => response.text())
+        .then(data => {
+            culture = data.split('\r\n');
+            console.log(culture);
+        })
+        .catch(error => console.error('Error loading the file:', error));
     //id and username
     let response = await fetch('https://graph.instagram.com/7091364407614663?fields=id,username&access_token=IGQWRQVGlxdHhGeVVWbk8yRllOdV8yWnBkcENFT0F1SzhPSFFIUmVFbzJWNlRlX3ZAsV1BDWHVkeUttU29jNnlUU0lUR01BYWY0OXg0TXdXWWUwY1YtaUFQNTA2OFpIVjNvMVpYbDcySGdvTU1pY2VDdk1uanU1aHMZD');
     
