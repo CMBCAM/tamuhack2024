d45e18d6c2d2e205e7af93ccc700d8b7fe68a6be
CMBCAM <cameron.barradas@gmail.com>
2024-01-27 20:25:34 -0600

    parse all words

--- index.js
+++ index.js
@@ -3,6 +3,56 @@ let name;
 let temp;
 let nextPage;
 let legitData;
+let yuckyPosts = [[]];
+
+function searchData(){
+    console.log(name);
+    //essentially go through each caption and look for keywords
+    //legitdata[i]id, media_type, media_url, permalink, timestamp
+
+    //Search through different types of data
+    /*
+    1. Phone numbers / email addresses general contact info ***
+    2. Birthday,  elementary / secondary school graduation years ***
+    3. interests/hobbies, **
+    4. family connections, occupation/title, etc **
+    5. vehicle **
+    6. home / address ***
+    7. vital government accounts etc ****
+    8. pets *
+    9. security question information
+    10.     What is your mother's maiden name?
+            What is the name of your first pet?
+            In what city were you born?
+            What is your favorite color?
+            What is the name of your high school?
+            What is your favorite movie or book?
+            What is your favorite sports team?
+            What is the model of your first car?
+            What is your favorite food?
+            What is the last name of your best childhood friend?
+    11. Recent activity / predictable schedule
+     recency in general
+    */
+    
+     //Size of array
+    let size = legitData.length ;
+    //Loop through elements
+    let wordArray;
+    let currentWord;
+    for (let i = 0; i < size; i++){
+        //check if a caption exists
+        if(legitData[i].caption !==undefined){
+            wordArray = legitData[i].caption.split(/\s+/);
+            for (let j = 0; j < wordArray.length;j++){
+                currentWord = wordArray[j]; //each word 
+                console.log(currentWord + ' ' + i);
+            }
+        }
+    }
+
+    console.log(size);
+}
 
 async function fetchData() {
   try {
@@ -52,11 +102,10 @@ async function fetchData() {
         legitData = legitData.concat(temp.data);
         console.log(legitData);
     }
-
+    searchData(); //up there!
   } catch (error) {
     console.error(error);
   }
 }
-
-// Call the function to initiate the fetch operation
+//initiate the fetch operation
 fetchData();
