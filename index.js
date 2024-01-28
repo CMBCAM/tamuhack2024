110bd766b927bf75a08dd30b0be7bbe6a8ac82ee
CMBCAM <cameron.barradas@gmail.com>
2024-01-27 19:22:07 -0600

    whole profile data is obtained

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
-let jsonData;
+let jsonData = {};
 let name;
+let temp;
+let nextPage;
+let legitData;
 
 async function fetchData() {
   try {
     //id and username
-    const response = await fetch('https://graph.instagram.com/7091364407614663?fields=id,username&access_token=IGQWRQVGlxdHhGeVVWbk8yRllOdV8yWnBkcENFT0F1SzhPSFFIUmVFbzJWNlRlX3ZAsV1BDWHVkeUttU29jNnlUU0lUR01BYWY0OXg0TXdXWWUwY1YtaUFQNTA2OFpIVjNvMVpYbDcySGdvTU1pY2VDdk1uanU1aHMZD');
+    let response = await fetch('https://graph.instagram.com/7091364407614663?fields=id,username&access_token=IGQWRQVGlxdHhGeVVWbk8yRllOdV8yWnBkcENFT0F1SzhPSFFIUmVFbzJWNlRlX3ZAsV1BDWHVkeUttU29jNnlUU0lUR01BYWY0OXg0TXdXWWUwY1YtaUFQNTA2OFpIVjNvMVpYbDcySGdvTU1pY2VDdk1uanU1aHMZD');
     
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
 
-    jsonData = await response.json();
-    console.log(jsonData);
+    temp = await response.json();
+    console.log(temp);
     //save name
-    name = jsonData.username;
+    name = temp.username;
+    console.log(name);
 
   } catch (error) {
     console.error(error);
   }
   //media
-  //captions might need to loop
+  //grab all the ids might need to loop
   try {
-    //captions
-    const response = await fetch('https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQWRQVGlxdHhGeVVWbk8yRllOdV8yWnBkcENFT0F1SzhPSFFIUmVFbzJWNlRlX3ZAsV1BDWHVkeUttU29jNnlUU0lUR01BYWY0OXg0TXdXWWUwY1YtaUFQNTA2OFpIVjNvMVpYbDcySGdvTU1pY2VDdk1uanU1aHMZD');
+    //rest of the data
+    let response = await fetch('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp,permalink&access_token=IGQWRQVGlxdHhGeVVWbk8yRllOdV8yWnBkcENFT0F1SzhPSFFIUmVFbzJWNlRlX3ZAsV1BDWHVkeUttU29jNnlUU0lUR01BYWY0OXg0TXdXWWUwY1YtaUFQNTA2OFpIVjNvMVpYbDcySGdvTU1pY2VDdk1uanU1aHMZD');
     
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
 
-    jsonData = await response.json();
-    console.log(jsonData);
+    temp = await response.json();
+    //console.log(temp);
 
+    legitData = temp.data;
+    console.log(legitData);
+    //now loop until next is finished
+    while(temp.paging.next !== undefined){
+        nextPage = temp.paging.next;
+        response = await fetch(nextPage);
     
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        temp = await response.json();
+        //console.log(temp);
+
+
+        legitData = legitData.concat(temp.data);
+        console.log(legitData);
+    }
+
   } catch (error) {
     console.error(error);
   }
