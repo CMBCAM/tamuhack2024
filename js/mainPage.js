174812375b21acff58d7fa8a1937ae4ef9962614
ProudOne7 <peternyen7@gmail.com>
2024-01-28 06:03:29 -0600

    Remove old funcs

--- js/mainPage.js
+++ js/mainPage.js
@@ -3,19 +3,12 @@ function diagDivAScroll(){
     const diagDivA = document.getElementById("diag-div-a");
     diagDivA.classList.add("diag-div-a-scrolled");
     diagDivComp();
-    diagDivPreComp();
 }
 
 function diagDivComp(){
     const diagDivA = document.getElementById("diag-div-comp-id");
     diagDivA.classList.add("diag-div-comp-reveal");
 }
-
-function diagDivPreComp(){
-    const diagDivPreComp = document.getElementById("diag-div-precomp-id");
-    diagDivPreComp.classList.add("diag-div-pre-comp-reveal");
-}
-
 
 var headerDesc = document.getElementById("header-desc-id");
 
